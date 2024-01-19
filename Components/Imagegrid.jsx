fabb0c6a1a778e9095da83b2b8adfef367cee664
lee14k <kmkovach@mtu.edu>
2024-01-19 00:33:38 -0500

    working on about page, contact page, homepage, finishes

--- Components/Imagegrid.jsx
+++ Components/Imagegrid.jsx
@@ -3,15 +3,18 @@ import Link from 'next/link';
 export default function Imagegrid({ images }) {
   return (
     <div className="gallerycontainer flex justify-center mb-10">
-      <div className="container ">
-        <div className="gallery ">
+      <div className="container">
+        <div className="gallery">
           {images.map((item, index) => (
-            <Link href={item.page} >
-                <div style={{ backgroundImage: `url(${item.link})` }} className={`flex justify-center items-center gallery__item gallery__item--${index + 1}`}>
+            <Link href={item.page} key={index}>
+              <div className="gallery__item-wrapper">
+                <div style={{ backgroundImage: `url(${item.link})` }} className={`gallery__item gallery__item--${index + 1}`}>
                   <figcaption className="gallery__caption text-4xl flex">
                     {item.header}
                   </figcaption>
                 </div>
+                <span className="mobile-caption text-4xl">{item.header}</span>
+              </div>
             </Link>
           ))}
         </div>
--- pages/About.js
+++ pages/About.js
@@ -1,9 +1,24 @@
+import Timeline from "@/Components/Timeline";
+import NewNav from "@/Components/NewNav";
+import Image from "next/image";
 const About = () => {
     return (
         <div>
-        <h1>About</h1>
-        <p>This is the about page</p>
+            <NewNav />
+            <div className="grid grid-cols-2 ml-10">
+                <Image
+                    src="/aboutpic.jpg"
+                    height={500}
+                    width={500}
+                />
+                <div className="flex flex-col justify-center items-center">
+            <h2>About Us</h2>
+            <p>Blurb about the company</p>
+                </div>
+            </div>
+            <h2>Our Process</h2>
+            <Timeline />
         </div>
     );
-    }
+}
 export default About
